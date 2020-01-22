e92944b8f32e6e0a17300756dc88e3656ceefe80
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-01-21 21:35:17 -0700

    added icon and local storage

--- js/index.js
+++ js/index.js
@@ -8,6 +8,19 @@ $.get('../markdown/TODO.md', function (data) {
     addToTextArea(data);
 }, 'text');
 
+// dark mode
+if (localStorage.dark == "0") {
+    $('#customSwitch1').trigger("click");
+    $('body').css("background-color", "#e6e6e6");
+}
+
 $('#customSwitch1').click(function() {
-    $('link[href="editor.md/css/editormd.min.css"').attr('href','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js');
+    if (localStorage.dark == "1") {
+        $('body').css("background-color", "#e6e6e6");
+        localStorage.dark = 0;
+    } else {
+        $('body').css("background-color", "#222");
+        localStorage.dark = 1;
+        console.log(localStorage.dark = 1);
+    }
 });
