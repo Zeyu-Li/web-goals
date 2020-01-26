733b7a161f002b7eadcefe58e5a2f68ffe65d08a
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-01-25 23:39:42 -0700

    finished delete 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,91 @@
 'use strict';
+var goals = localStorage.goals;
+var fgoals = localStorage.fgoals;
+
+// get completed and goals that need to be done
+$(document).ready(function () {
+    get_data();
+});
+
+// data for info
+function get_data() {
+    goals = goals.split(',');
+
+    var split_goal = [];
+    console.log(goals);
+    for (var goal of goals) {
+        split_goal.push(goal.split(';').reverse());
+    }
+    split_goal = split_goal.sort().reverse();
+    $('.goals').empty();
+    var i = 0;
+    for (var to_print_goal of split_goal) {
+
+        if (to_print_goal[0] =="") {
+            break;
+        }
+
+        let date = to_print_goal[0];
+        var dob = new Date(date);
+        var dobArr = dob.toDateString().split(' ');
+        var dobFormat = dobArr[1] + ' ' + dobArr[2]  + ' ' + dobArr[3];
+        $('.goals').prepend(
+            `<div class="ind_goal" id="goal_number_`+i+`">
+                <div class="check_box"><input class="main_box goal_`+i+`" type="checkbox" name="goal`+i+`" value="goal`+i+`" style="float: left;"></div>
+                <div class="txt">
+                    <p>` + to_print_goal[1] + `<br><br>
+                    <b>DUE: `+dobFormat+`</b></p>
+                </div>
+                <div class="delete">
+                    <a id="de_goals`+i+`" class="trashy" title="Delete Goals" style="z-index: 100;">
+                        <button type="button" class="btn btn-light trash delete`+i+`">
+                            <img src="images/trash_bin.svg" id="trash_can">
+                        </button>
+                    </a>
+                </div>
+                <script>
+                    $('#de_goals`+i+`').click(function() {
+                        delete_goals("`+to_print_goal[1]+`","`+to_print_goal[0]+`");
+                    });
+                    $('.goal_`+i+`').click(function() {
+                        // check box
+                        $('#goal_number_`+ i +`').css('background-color', 'green');
+                        delete_goals("`+to_print_goal[1]+`","`+to_print_goal[0]+`");
+                    });
+                </script>
+            </div>`
+        );
+        i++;
+    }
+}
+
+function find_goals(goals, to_delete) {
+    let j = 0;
+    for (var goal of goals) {
+        if (goal == to_delete) {
+            return j;
+        };
+        j++;
+    }
+}
+
+function delete_goals(message, datetime) {
+    // add goals to screen and storage
+    var to_delete = [message+';'+datetime];
+    var index = find_goals(goals, to_delete);
+    goals.pop(index);
+    localStorage.goals = goals;
+    goals = localStorage.goals;
+    get_data();
+}
+
+function add_goals(message, datetime) {
+    // add goals to screen and storage
+    goals.push([message+';'+datetime]);
+    localStorage.goals = goals;
+    goals = localStorage.goals;
+    get_data();
+}
 
 // from data in json, copy to textarea
 function addToTextArea(data) {
@@ -11,10 +98,14 @@ $.get('../markdown/TODO.md', function (data) {
 }, 'text');
 
 /* Start of buttons */
-$('.main_box').click(function() {
-    // check box
-    $('#ind_goal').css('background-color', 'green');
+
+// events
+
+$('.add_event').click(function() {
+    add_goals($('#message-text').val(), $('#datetimepicker').val());
 });
+
+// end of events
 
 // add event
 $('#first button').click(function() {
@@ -22,7 +113,14 @@ $('#first button').click(function() {
     $('#datetimepicker').datetimepicker();
 });
 
-$('#second button').click(function(e) {
+$('.d_goals button').click(function() {
+    // download markdown file
+    var data = new Blob([localStorage.goals], {type: 'text/plain;charset=utf-8'});
+    let textFile = URL.createObjectURL(data);
+    $('.d_goals a').attr('href', textFile);
+});
+
+$('#second button').click(function() {
     // download markdown file
     var data = new Blob([$('#editor textarea').val()], {type: 'text/plain;charset=utf-8'});
     let textFile = URL.createObjectURL(data);
@@ -70,4 +168,11 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this)
     modal.find('.modal-title').text('Event')
+});
+
+// back to top
+$(".backtotop").click(function() {
+    return $("html, body").animate({
+        scrollTop: 0
+    }, 300), !1;
 });
