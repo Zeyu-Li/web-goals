6441ef2eeffd217c672cffa8b8b9e5d11795b248
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-01-27 08:44:02 -0700

    marked up files

--- js/index.js
+++ js/index.js
@@ -1,30 +1,39 @@
 'use strict';
 
+// global inits
 var goals = localStorage.goals;
 var fgoals = localStorage.fgoals;
 
 // data for info
 function get_data() {
+    // init var
     let color;
-    goals = goals.split(',');
 
-    var split_goal = [];
-    console.log(goals);
+    // parse through goals
+    goals = goals.split(',');
+    let split_goal = [];
     for (var goal of goals) {
         split_goal.push(goal.split(';').reverse());
     }
     split_goal = split_goal.sort().reverse();
+
+    // empties goals class to build new goals
     $('.goals').empty();
+
+    // for each goal in formatted goal, 
+    // if it is trivial, don't print anything, otherwise 
+    // print the goals out
     let i = 0;
     for (var to_print_goal of split_goal) {
-
         if (to_print_goal[0] =="") {
             break;
         }
 
+        // formate date
         let date = to_print_goal[0];
         let dob = new Date(date);
         if (dob.getTime() <= $.now()) {
+            // if it is past due, color it yellow
             color = "background-color:rgba(255, 243, 112,0.3)";
         }
         let dobArr = dob.toDateString().split(' ');
@@ -38,7 +47,6 @@ function get_data() {
                 </div>
                 <script>
                     $('.goal_`+i+`').click(function() {
-                        // check box
                         $('#goal_number_`+ i +`').css('background-color', 'green');
                         delete_goals("`+to_print_goal[1]+`","`+to_print_goal[0]+`");
                     });
@@ -50,6 +58,7 @@ function get_data() {
 }
 
 function find_goals(goals, to_delete) {
+    // find if var to delete is found in goals array
     let j = 0;
     for (var goal of goals) {
         if (goal == to_delete) {
@@ -60,6 +69,7 @@ function find_goals(goals, to_delete) {
 }
 
 function finished() {
+    // 
     fgoals = fgoals.split(',');
 
     var split_f_goal = [];
@@ -94,7 +104,7 @@ function finished() {
 }
 
 function delete_goals(message, datetime) {
-    // add goals to screen and storage
+    // delete goal from screen and storage
     var to_delete = [message+';'+datetime];
     var index = find_goals(goals, to_delete);
     let to_done = goals.pop(index);
@@ -105,7 +115,7 @@ function delete_goals(message, datetime) {
 }
 
 function add_f_goals(to_done) {
-    // add goals to screen and storage
+    // add finished goals to finished screen and storage
     fgoals.push([to_done]);
     localStorage.fgoals = fgoals;
     fgoals = localStorage.fgoals;
@@ -120,29 +130,28 @@ function add_goals(message, datetime) {
     get_data();
 }
 
-// from data in json, copy to textarea
 function addToTextArea(data) {
+    // from data in json, copy to textarea
     $('#editor textarea').val(data);
 }
 
-// from data in json, copy to textarea
 $.get('../markdown/TODO.md', function (data) {
+    // from data in json, copy to textarea
     addToTextArea(data);
 }, 'text');
 
 /* Start of buttons */
 
 // events
-
 $('.add_event').click(function() {
+    // call add event
     add_goals($('#message-text').val(), $('#datetimepicker').val());
 });
-
 // end of events
 
 // add event
 $('#first button').click(function() {
-    // TODO: trigger prompt
+    // get datetime
     $('#datetimepicker').datetimepicker();
 });
 
@@ -160,14 +169,14 @@ $('#second button').click(function() {
     $('#second a').attr('href', textFile);
 });
 
-// when copy button is clicked, the text in textarea is copied to clipboard 
 $(".copy button").click(function() {
+    // when copy button is clicked, the text in textarea is copied to clipboard 
     $(".copy button").addClass("btn-success");
     $(".copy button").text("Copied");
     navigator.clipboard.writeText($('#editor textarea').val());
 })
 
-// toggle dark mode
+// toggle dark mode on load
 if (localStorage.dark == "0") {
     $('#customSwitch1').trigger("click");
     $('body').css("background-color", "#e6e6e6");
@@ -211,8 +220,9 @@ $(".backtotop").click(function() {
     }, 300), !1;
 });
 
-// get completed and goals that need to be done
 $(document).ready(function () {
+    // get completed and goals that need to be done
+    // on load
     get_data();
     finished();
 });
